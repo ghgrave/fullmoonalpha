f75290c0c57cf2c9dfa8c34b12d329ee4a40015a
Jon-Mikel <garrisonhgrave@yahoo.com>
2022-02-24 17:27:01 -0600

    added fake image data and tweaked route to filter out null image objects

--- api/controllers/imagesControllers.js
+++ api/controllers/imagesControllers.js
@@ -1,5 +1,25 @@
-const {images} = require('../../data/fakeImageData')
+// const {images} = require('../../data/fakeImageData')
+const { imageInfo } = require("../../data/fakeImageInfo");
 
 exports.displayImagesPage = (req, res) => {
-  res.render("images", {images});
-}
+  // creates temp array of elements filtering out those without images
+  let tempImageInfo = imageInfo.filter(el => {
+    return el.image !== null
+  })
+  // takes length of temp arr and finds remainder
+  // uses this to "fill" the rest of array to multiple of 5
+  let filler = 5 - (tempImageInfo.length % 5);
+  // fills the array out with null data
+  for (let index = 0; index < filler; index++) {
+    let fillerData = {
+      name: null,
+      image: null,
+      media: null,
+      description: null,
+      poster: null,
+      link: null
+    }
+    tempImageInfo.push(fillerData);
+  }
+  res.render("images", { images: tempImageInfo });
+};
---
+++ data/fakeImageInfo.js
@@ -0,0 +1,102 @@
+exports.imageInfo = [
+  {
+    name: "Meridian",
+    image: "assets/images/delete/meridian.jpeg",
+    media: "movie",
+    description:
+      "Catherine Bomarzini returns to the family castle in Italy after her father's death and gets caught in the web of a mysterious love triangle: a man who is at times repulsive, at others enchanting; and a creature of the night whose gentle eyes and touch reveal his infinitive love and devotion. With the help of Martha, her faithful childhood nanny, and the ghost of a slain young girl, Catherine discovers the medieval curse that threatens their lives and only she can dispel.",
+    poster: null,
+    link: null
+  },
+  {
+    name: "Teen Wolf",
+    image: "assets/images/delete/teen_wolf_movie.jpeg",
+    media: "movie",
+    description:
+      "When a shy teenager's new-found powers help him score at basketball - and with the popular girls - he has some pretty hairy decisions to make.",
+    poster: null,
+    link: null
+  },
+  {
+    name: "Teen Wolf",
+    image: "assets/images/delete/teen_wolf_tv.jpeg",
+    media: "tv",
+    description:
+      "Scott McCall, a high school student living in the town of Beacon Hills has his life drastically changed when he's bitten by a werewolf, becoming one himself. He must henceforth learn to balance his problematic new identity with his day-to-day teenage life. The following characters are instrumental to his struggle: Stiles, his best friend; Allison, his love interest who comes from a family of werewolf hunters; and Derek, a mysterious werewolf with a dark past. Throughout the series, he strives to keep his loved ones safe while maintaining normal relationships with them.",
+    poster: null,
+    link: null
+  },
+  {
+    name: "Bitten",
+    image: "assets/images/delete/bitten_tv.jpeg",
+    media: "tv",
+    description:
+      "Scott McCall, a high school student living in the town of Beacon Hills has his life drastically changed when he's bitten by a werewolf, becoming one himself. He must henceforth learn to balance his problematic new identity with his day-to-day teenage life. The following characters are instrumental to his struggle: Stiles, his best friend; Allison, his love interest who comes from a family of werewolf hunters; and Derek, a mysterious werewolf with a dark past. Throughout the series, he strives to keep his loved ones safe while maintaining normal relationships with them.",
+    poster: null,
+    link: null
+  },
+  {
+    name: "Scary Werewolf",
+    image: "assets/images/delete/scaryWerewolf.jpg",
+    media: "art",
+    description: null,
+    poster: null,
+    link: null
+  },
+  {
+    name: "Werewolf in Jeans",
+    image: "assets/images/delete/werewolf.jpg",
+    media: "art",
+    description: null,
+    poster: null,
+    link: null
+  },
+  {
+    name: "Night of the Werewolf",
+    image: null,
+    media: "video",
+    description: "Halloween music about a haunted town where the villagers always make sure to be in their homes around sunset, for a werewolf is said to come out of the forest and enter into the town as soon as it gets dark outside. This music that I composed is called Night of the Werewolf.",
+    poster: null,
+    link: "https://youtu.be/FOSOcDvbcyc"
+  },
+  {
+    name: "Werwolf",
+    image: null,
+    media: "video",
+    description: "Werwolf (with intro) by a group called Absurd",
+    poster: null,
+    link: "https://youtu.be/87pWWyWfsi8"
+  },
+  {
+    name: "Growling Werewolf",
+    image: "assets/images/delete/werewolf-2.jpeg",
+    media: "art",
+    description: "Growling werewolf with extended claws",
+    poster: null,
+    link: null
+  },
+  {
+    name: "Werewolf",
+    image: "assets/images/delete/werewolf-3.jpeg",
+    media: "art",
+    description: "Werewolf raising arms in front of a full moon. Looks like he has been shot.",
+    poster: null,
+    link: null
+  },
+  {
+    name: "Werewolf by Night",
+    image: "assets/images/delete/werewolf-4.jpeg",
+    media: "art",
+    description: "Artwork from the comic Werewolf by Night.",
+    poster: null,
+    link: null
+  },
+  {
+    name: "Curse of the Werewolf",
+    image: "assets/images/delete/werewolf-5.jpeg",
+    media: "movie",
+    description: "Actor Oliver Reed in the 1961 release of The Curse of the Werewolf.",
+    poster: null,
+    link: null
+  },
+];
