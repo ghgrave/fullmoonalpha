6371bc9f833bad5c55c73b223d1555519a590b1f
Jon-Mikel <garrisonhgrave@yahoo.com>
2022-03-07 20:44:05 -0600

    able to submit form to correct devs/upload route with post

--- api/controllers/devhomeControllers.js
+++ api/controllers/devhomeControllers.js
@@ -3,3 +3,7 @@ const controls = require('../../data/devImageFormData')
 exports.displayHomePage = (req, res) => {
   res.render("devhome", {controls});
 };
+
+exports.uploadImage = (req, res) => {
+  console.log("So far so good")
+}
--- api/routes/devhomeRoutes.js
+++ api/routes/devhomeRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const devhome = require("../controllers/devhomeControllers");
 
 router.get("/", devhome.displayHomePage);
+router.post("/upload", devhome.uploadImage);
 
 module.exports = router;
